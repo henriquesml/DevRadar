c14a3f15611c74d00491fbeec5b9d6ab9dc42b57
henriquesml <henrique_schmeller@hotmail.com>
2020-01-16 13:32:36 -0300

    continuação do frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,146 @@
-import React from 'react';
+import React,{ useEffect, useState } from 'react';
+
 import './global.css'
 import './App.css'
+import './Sidebar.css'
+import './Main.css'
 
 function App() {
+
+  const [github_username, setGithub_username] = useState('')
+  const [techs, setTechs] = useState('')
+
+  const [latitude, setLatitude] = useState('')
+  const [longitude, setLongitude] = useState('')
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords
+        setLatitude(latitude)
+        setLongitude(longitude)
+
+      }, (err) => {
+        console.log(err)
+      },
+      {
+        timeout: 30000,
+      }
+
+    )
+  }, [])
+
+  async function handleAddDev(e){
+    e.preventDefault();
+  }
+
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <from>
-          <label htmlFor="user">Usuário do Github</label>
-        </from>
+        <form onSubmit={handleAddDev} >
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input 
+              name="github_username" 
+              id="github_username" 
+              required 
+              value={github_username}
+              onChange={e => setGithub_username(e.target.value)}
+            />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input 
+              name="techs" 
+              id="techs" 
+              required 
+              value={techs}
+              onChange={e => setTechs(e.target.value)}
+            />
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input 
+                type="number" 
+                name="latitude" 
+                id="latitude" 
+                required 
+                value={latitude}
+                onChange={ e => setLatitude(e.target.value) }
+              />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input 
+                type="number" 
+                name="longitude" 
+                id="longitude" 
+                required 
+                value={longitude} 
+                onChange={ e => setLongitude(e.target.value) }
+              />
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
       </aside>
 
       <main>
+        <ul>
+          <li className='dev-item'>
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/39440032?s=460&v=4" alt="Henrique Schmeller"></img>
+              <div className="user-info" >
+                <strong>Henrique Schmeller</strong>
+                <span>ReactJS, React Native, NodeJS</span>
+              </div>
+            </header>
+            <p>Apaixonado por desenvolvimento</p>
+            <a href='https://github.com/henriquesml'>Acessar perfil no Github</a>
+          </li>
 
+          <li className='dev-item'>
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/39440032?s=460&v=4" alt="Henrique Schmeller"></img>
+              <div className="user-info" >
+                <strong>Henrique Schmeller</strong>
+                <span>ReactJS, React Native, NodeJS</span>
+              </div>
+            </header>
+            <p>Apaixonado por desenvolvimento</p>
+            <a href='https://github.com/henriquesml'>Acessar perfil no Github</a>
+          </li>
+
+          <li className='dev-item'>
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/39440032?s=460&v=4" alt="Henrique Schmeller"></img>
+              <div className="user-info" >
+                <strong>Henrique Schmeller</strong>
+                <span>ReactJS, React Native, NodeJS</span>
+              </div>
+            </header>
+            <p>Apaixonado por desenvolvimento</p>
+            <a href='https://github.com/henriquesml'>Acessar perfil no Github</a>
+          </li>
+
+          <li className='dev-item'>
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/39440032?s=460&v=4" alt="Henrique Schmeller"></img>
+              <div className="user-info" >
+                <strong>Henrique Schmeller</strong>
+                <span>ReactJS, React Native, NodeJS</span>
+              </div>
+            </header>
+            <p>Apaixonado por desenvolvimento</p>
+            <a href='https://github.com/henriquesml'>Acessar perfil no Github</a>
+          </li>
+        </ul>
       </main>
     </div>
   );
