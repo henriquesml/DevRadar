c33028784cbe4a81c146590cceddb8655c62e514
henriquesml <henrique_schmeller@hotmail.com>
2020-01-16 08:19:19 -0300

    iniciando o frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './global.css'
+import './App.css'
+
+function App() {
+  return (
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <from>
+          <label htmlFor="user">Usuário do Github</label>
+        </from>
+      </aside>
+
+      <main>
+
+      </main>
+    </div>
+  );
+}
+
+export default App;
